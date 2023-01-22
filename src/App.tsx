7ca3eef2dc406437a2236cebd0c80556e3c59e2c
Jacek Gizinski <jacektrg+github@gmail.com>
2023-01-22 19:16:14 +0100

    Adding routing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import './App.scss';
+import { Routes, Route, Link } from 'react-router-dom';
+import Send from './components/Send';
+import Receive from './components/Receive';
+import Code from './components/Code';
 
-function App() {
-	return <div className='app'></div>;
-}
+const App = () => {
+	return (
+		<div className='app'>
+			<Routes>
+				<Route
+					path=''
+					element={
+						<>
+							<Link to='send'>SEND</Link>
+							<Link to='receive'>RECEIVE</Link>
+						</>
+					}
+				/>
+				<Route path='send' element={<Send />} />
+				<Route path='receive' element={<Receive />} />
+				<Route path='code/:dirId' element={<Code />} />
+			</Routes>
+		</div>
+	);
+};
 
 export default App;
