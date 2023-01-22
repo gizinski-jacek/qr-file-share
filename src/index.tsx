bd3f16975a1b21b45ece44e0d8d3a25212eaa9b4
Jacek Gizinski <jacektrg+github@gmail.com>
2023-01-22 19:16:19 +0100

    Adding routing.

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './reset.css';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</React.StrictMode>
 );
 
