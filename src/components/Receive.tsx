1032ec2489493afb73d4ecaca3d50b85be7a5009
Jacek Gizinski <jacektrg+github@gmail.com>
2023-01-23 23:02:53 +0100

    Fixing react env vars.
    Removing Link.

--- src/components/Receive.tsx
+++ src/components/Receive.tsx
@@ -1,8 +1,8 @@
-// import "./styles.scss";
+import '../App.scss';
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { QRCodeSVG } from 'qrcode.react';
 import axios from 'axios';
+import { io } from 'socket.io-client';
 
 const Receive = () => {
 	const [dirId, setDirId] = useState<string>('');
@@ -11,7 +11,7 @@ const Receive = () => {
 		(async () => {
 			try {
 				const res = await axios.get(
-					`${process.env.API_URI}/receive-single-file/${dirId}`
+					`${process.env.REACT_APP_API_URI}/receive-single-file/${dirId}`
 				);
 				setDirId(res.data);
 			} catch (error) {
@@ -20,12 +20,23 @@ const Receive = () => {
 		})();
 	}, [dirId]);
 
+	// useEffect(() => {
+	//   (() => {
+	//     // Create socket connection with dirId
+	//     // to get notified of uploaded file.
+	//     const socket = io();
+	//   })();
+	// }, [dirId]);
+
 	return (
 		<div className='receive'>
 			<h1>Hello receive</h1>
-			{dirId && <QRCodeSVG value={`${process.env.CLIENT_URI}/code/${dirId}`} />}
+			{dirId && (
+				<QRCodeSVG
+					value={`${process.env.REACT_APP_CLIENT_URI}/code/${dirId}`}
+				/>
+			)}
 			<div>{dirId}</div>
-			<Link to='/'>{'<- Home'}</Link>
 		</div>
 	);
 };
