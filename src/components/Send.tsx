d8c33b6ed53657d81d79e1bbadaa109d3355b46e
Jacek Gizinski <jacektrg+github@gmail.com>
2023-01-25 23:25:01 +0100

    Adding svg icons

--- src/components/Send.tsx
+++ src/components/Send.tsx
@@ -8,7 +8,7 @@ const Send = () => {
 	const [fileURL, setFileURL] = useState<string | null>();
 	const ref = useRef<HTMLInputElement>(null);
 
-	const clickSelectFile = (e: React.MouseEvent<HTMLImageElement>) => {
+	const clickSelectFile = (e: React.MouseEvent<HTMLDivElement>) => {
 		e.stopPropagation();
 		ref.current?.click();
 	};
@@ -31,7 +31,7 @@ const Send = () => {
 			const profileData = new FormData();
 			profileData.append('file', fileData);
 			const res = await axios.post(
-				`${process.env.REACT_APP_API_URI_DEV}/api/send-single-file`,
+				`${process.env.REACT_APP_API_URI}/api/send-single-file`,
 				profileData
 			);
 			setFileData(null);
@@ -43,8 +43,6 @@ const Send = () => {
 
 	return (
 		<div className='send'>
-			{fileURL && <QRCodeSVG value={fileURL} />}
-			<img src='' alt='select file' onClick={clickSelectFile} />
 			<form>
 				<label htmlFor='file'>Select file</label>
 				<input
@@ -55,8 +53,56 @@ const Send = () => {
 					onChange={handleFileSelect}
 				/>
 			</form>
-			<button type='button' onClick={handleFileUpload}>
-				send
+			{fileURL && <QRCodeSVG value={fileURL} />}
+			{!fileURL && fileData ? (
+				<div
+					className='checkmark-icon'
+					onClick={clickSelectFile}
+					aria-label='file selected'
+				>
+					<svg
+						viewBox='0 0 24 24'
+						fill='none'
+						xmlns='http://www.w3.org/2000/svg'
+					>
+						<path
+							d='M7.5 12L10.5 15L16.5 9M7.8 21H16.2C17.8802 21 18.7202 21 19.362 20.673C19.9265 20.3854 20.3854 19.9265 20.673 19.362C21 18.7202 21 17.8802 21 16.2V7.8C21 6.11984 21 5.27976 20.673 4.63803C20.3854 4.07354 19.9265 3.6146 19.362 3.32698C18.7202 3 17.8802 3 16.2 3H7.8C6.11984 3 5.27976 3 4.63803 3.32698C4.07354 3.6146 3.6146 4.07354 3.32698 4.63803C3 5.27976 3 6.11984 3 7.8V16.2C3 17.8802 3 18.7202 3.32698 19.362C3.6146 19.9265 4.07354 20.3854 4.63803 20.673C5.27976 21 6.11984 21 7.8 21Z'
+							stroke='#fff'
+							stroke-width='2'
+							stroke-linecap='round'
+							stroke-linejoin='round'
+						/>
+					</svg>
+				</div>
+			) : (
+				<div
+					className='add-icon'
+					onClick={clickSelectFile}
+					aria-label='select file to upload'
+				>
+					<svg
+						viewBox='0 0 24 24'
+						fill='none'
+						xmlns='http://www.w3.org/2000/svg'
+					>
+						<path
+							d='M12 8V16M8 12H16M7.8 21H16.2C17.8802 21 18.7202 21 19.362 20.673C19.9265 20.3854 20.3854 19.9265 20.673 19.362C21 18.7202 21 17.8802 21 16.2V7.8C21 6.11984 21 5.27976 20.673 4.63803C20.3854 4.07354 19.9265 3.6146 19.362 3.32698C18.7202 3 17.8802 3 16.2 3H7.8C6.11984 3 5.27976 3 4.63803 3.32698C4.07354 3.6146 3.6146 4.07354 3.32698 4.63803C3 5.27976 3 6.11984 3 7.8V16.2C3 17.8802 3 18.7202 3.32698 19.362C3.6146 19.9265 4.07354 20.3854 4.63803 20.673C5.27976 21 6.11984 21 7.8 21Z'
+							stroke='#fff'
+							stroke-width='2'
+							stroke-linecap='round'
+							stroke-linejoin='round'
+						/>
+					</svg>
+				</div>
+			)}
+			{fileData && <div>File: {fileData.name}</div>}
+			<button
+				className='send-btn'
+				type='button'
+				onClick={handleFileUpload}
+				disabled={!fileData}
+			>
+				Send
 			</button>
 		</div>
 	);
