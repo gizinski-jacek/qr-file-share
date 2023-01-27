707bb433a00706e0bad8af3f9a0fc03b3ab87bbc
Jacek Gizinski <jacektrg+github@gmail.com>
2023-01-27 01:11:46 +0100

    Fixing svg properties.
    Receiving outlet context value.
    Changing fileData state to fileList to support multiple files.
    Adding state for file errors and rendering them appropriately.
    Adjusting file handlers to support multiple files.

--- src/components/Send.tsx
+++ src/components/Send.tsx
@@ -1,40 +1,53 @@
-import '../App.scss';
+import '../styles/Send.scss';
 import { useRef, useState } from 'react';
 import axios from 'axios';
 import { QRCodeSVG } from 'qrcode.react';
+import { useSingleMode } from './Overlay';
 
 const Send = () => {
-	const [fileData, setFileData] = useState<File | null>();
+	const [fileList, setFileList] = useState<FileList | null>();
+	const [fileListErrors, setFileListErrors] = useState<string[] | null>(null);
 	const [fileURL, setFileURL] = useState<string | null>();
 	const ref = useRef<HTMLInputElement>(null);
+	const { singleMode } = useSingleMode();
 
 	const clickSelectFile = (e: React.MouseEvent<HTMLDivElement>) => {
 		e.stopPropagation();
 		ref.current?.click();
 	};
 
-	const handleFileSelect = (e: React.ChangeEvent) => {
+	const handleFileSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
 		e.stopPropagation();
-		const target = e.target as HTMLInputElement;
-		const file = (target.files as FileList)[0];
-		if (file.size > 2000000) {
-			alert('File too big');
+		const files = e.target.files as FileList;
+		const errors: string[] = [];
+		for (let i = 0; i < files.length; i++) {
+			const file = files[i];
+			if (file.size > 2000000) {
+				errors.push(`${file.name} file is too big (max. 2Mb)`);
+			}
+		}
+		if (errors.length) {
+			setFileList(null);
+			setFileListErrors(errors);
 			return;
 		}
-		setFileData(file);
+		setFileListErrors(null);
+		setFileList(files);
 	};
 
 	const handleFileUpload = async (e: React.MouseEvent<HTMLButtonElement>) => {
 		e.preventDefault();
 		try {
-			if (!fileData) return;
-			const profileData = new FormData();
-			profileData.append('file', fileData);
+			if (!fileList) return;
+			const uploadData = new FormData();
+			for (let i = 0; i < fileList.length; i++) {
+				uploadData.append(`file-${i}`, fileList[i]);
+			}
 			const res = await axios.post(
 				`${process.env.REACT_APP_API_URI}/api/send-single-file`,
-				profileData
+				uploadData
 			);
-			setFileData(null);
+			setFileList(null);
 			setFileURL(res.data);
 		} catch (error) {
 			console.log(error);
@@ -43,18 +56,23 @@ const Send = () => {
 
 	return (
 		<div className='send'>
-			<form>
+			<form encType='multipart/form-data'>
 				<label htmlFor='file'>Select file</label>
 				<input
 					ref={ref}
 					id='file'
 					name='file'
 					type='file'
+					multiple={!singleMode}
 					onChange={handleFileSelect}
 				/>
 			</form>
-			{fileURL && <QRCodeSVG value={fileURL} />}
-			{!fileURL && fileData ? (
+			{fileURL && (
+				<div className='qr-code' aria-label='qr code'>
+					<QRCodeSVG value={fileURL} />
+				</div>
+			)}
+			{fileURL ? null : fileList ? (
 				<div
 					className='checkmark-icon'
 					onClick={clickSelectFile}
@@ -68,9 +86,9 @@ const Send = () => {
 						<path
 							d='M7.5 12L10.5 15L16.5 9M7.8 21H16.2C17.8802 21 18.7202 21 19.362 20.673C19.9265 20.3854 20.3854 19.9265 20.673 19.362C21 18.7202 21 17.8802 21 16.2V7.8C21 6.11984 21 5.27976 20.673 4.63803C20.3854 4.07354 19.9265 3.6146 19.362 3.32698C18.7202 3 17.8802 3 16.2 3H7.8C6.11984 3 5.27976 3 4.63803 3.32698C4.07354 3.6146 3.6146 4.07354 3.32698 4.63803C3 5.27976 3 6.11984 3 7.8V16.2C3 17.8802 3 18.7202 3.32698 19.362C3.6146 19.9265 4.07354 20.3854 4.63803 20.673C5.27976 21 6.11984 21 7.8 21Z'
 							stroke='#fff'
-							stroke-width='2'
-							stroke-linecap='round'
-							stroke-linejoin='round'
+							strokeWidth='2'
+							strokeLinecap='round'
+							strokeLinejoin='round'
 						/>
 					</svg>
 				</div>
@@ -88,19 +106,33 @@ const Send = () => {
 						<path
 							d='M12 8V16M8 12H16M7.8 21H16.2C17.8802 21 18.7202 21 19.362 20.673C19.9265 20.3854 20.3854 19.9265 20.673 19.362C21 18.7202 21 17.8802 21 16.2V7.8C21 6.11984 21 5.27976 20.673 4.63803C20.3854 4.07354 19.9265 3.6146 19.362 3.32698C18.7202 3 17.8802 3 16.2 3H7.8C6.11984 3 5.27976 3 4.63803 3.32698C4.07354 3.6146 3.6146 4.07354 3.32698 4.63803C3 5.27976 3 6.11984 3 7.8V16.2C3 17.8802 3 18.7202 3.32698 19.362C3.6146 19.9265 4.07354 20.3854 4.63803 20.673C5.27976 21 6.11984 21 7.8 21Z'
 							stroke='#fff'
-							stroke-width='2'
-							stroke-linecap='round'
-							stroke-linejoin='round'
+							strokeWidth='2'
+							strokeLinecap='round'
+							strokeLinejoin='round'
 						/>
 					</svg>
 				</div>
 			)}
-			{fileData && <div>File: {fileData.name}</div>}
+			{fileListErrors && (
+				<div>
+					{fileListErrors.map((error, i) => (
+						<div key={i}>{error}</div>
+					))}
+				</div>
+			)}
+			{fileList && (
+				<div>
+					Files:{' '}
+					{Array.from(fileList).map((file, i) => {
+						return <div key={i}>{file.name}</div>;
+					})}
+				</div>
+			)}
 			<button
 				className='send-btn'
 				type='button'
 				onClick={handleFileUpload}
-				disabled={!fileData}
+				disabled={!fileList || !!fileListErrors}
 			>
 				Send
 			</button>
