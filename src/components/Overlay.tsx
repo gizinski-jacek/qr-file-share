38bd48f416684f69b30a81d16e9277f5bed36196
Jacek Gizinski <jacektrg+github@gmail.com>
2023-01-27 01:08:17 +0100

    Fixing svg properties.
    Adding state and handlers to toggle between uploading single or multiple files.
    Using outlet context to pass value to child components

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,37 +1,70 @@
-import { NavLink, Outlet } from 'react-router-dom';
-import '../App.scss';
+import '../styles/Overlay.scss';
+import { useState } from 'react';
+import { NavLink, Outlet, useOutletContext } from 'react-router-dom';
+
+interface ContextType {
+	singleMode: boolean;
+}
 
 const Overlay = () => {
+	const [singleMode, setSingleMode] = useState<boolean>(true);
+
+	const setSingle = () => setSingleMode(true);
+
+	const setMultiple = () => setSingleMode(false);
+
 	return (
 		<>
-			<NavLink className='home-link' to=''>
-				<div className='arrow-icon' aria-label='home page link'>
-					<svg
-						viewBox='0 0 24 24'
-						fill='none'
-						xmlns='http://www.w3.org/2000/svg'
+			<Outlet context={{ mode: singleMode }} />
+			<div className='overlay'>
+				<NavLink className='home-link' to=''>
+					<div className='arrow-icon' aria-label='home page link'>
+						<svg
+							viewBox='0 0 24 24'
+							fill='none'
+							xmlns='http://www.w3.org/2000/svg'
+						>
+							<path
+								d='M9.57 5.93018L3.5 12.0002L9.57 18.0702'
+								stroke='#fff'
+								strokeWidth='2'
+								strokeLinecap='round'
+								strokeLinejoin='round'
+							/>
+							<path
+								d='M20.4999 12H3.66992'
+								stroke='#fff'
+								strokeWidth='2'
+								strokeLinecap='round'
+								strokeLinejoin='round'
+							/>
+						</svg>
+					</div>
+					Home
+				</NavLink>
+				<div className='modes'>
+					<button
+						className={singleMode ? 'active' : ''}
+						type='button'
+						onClick={setSingle}
 					>
-						<path
-							d='M9.57 5.93018L3.5 12.0002L9.57 18.0702'
-							stroke='#fff'
-							stroke-width='2'
-							stroke-linecap='round'
-							stroke-linejoin='round'
-						/>
-						<path
-							d='M20.4999 12H3.66992'
-							stroke='#fff'
-							stroke-width='2'
-							stroke-linecap='round'
-							stroke-linejoin='round'
-						/>
-					</svg>
+						Single
+					</button>
+					<button
+						className={!singleMode ? 'active' : ''}
+						type='button'
+						onClick={setMultiple}
+					>
+						Multiple
+					</button>
 				</div>
-				Home
-			</NavLink>
-			<Outlet />
+			</div>
 		</>
 	);
 };
+
+export function useSingleMode() {
+	return useOutletContext<ContextType>();
+}
 
 export default Overlay;
