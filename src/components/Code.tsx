166f4450bb8b8ce6729064d28b8bc60c81e7cb91
Jacek Gizinski <jacektrg+github@gmail.com>
2023-01-27 01:10:24 +0100

    Fixing svg properties.
    Receiving outlet context value

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -1,13 +1,15 @@
-import '../App.scss';
+import '../styles/Code.scss';
 import axios from 'axios';
 import { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { io } from 'socket.io-client';
+import { useSingleMode } from './Overlay';
 
 const Code = () => {
 	const { dirId } = useParams();
 	const [fileData, setFileData] = useState<File | null>();
 	const ref = useRef<HTMLInputElement>(null);
+	const { singleMode } = useSingleMode();
 
 	useEffect(() => {
 		(async () => {
@@ -56,13 +58,14 @@ const Code = () => {
 
 	return !dirId ? null : (
 		<div className='send'>
-			<form>
+			<form encType='multipart/form-data'>
 				<label htmlFor='file'>Select file</label>
 				<input
 					ref={ref}
 					id='file'
 					name='file'
 					type='file'
+					multiple={!singleMode}
 					onChange={handleFileSelect}
 				/>
 			</form>
@@ -80,9 +83,9 @@ const Code = () => {
 						<path
 							d='M7.5 12L10.5 15L16.5 9M7.8 21H16.2C17.8802 21 18.7202 21 19.362 20.673C19.9265 20.3854 20.3854 19.9265 20.673 19.362C21 18.7202 21 17.8802 21 16.2V7.8C21 6.11984 21 5.27976 20.673 4.63803C20.3854 4.07354 19.9265 3.6146 19.362 3.32698C18.7202 3 17.8802 3 16.2 3H7.8C6.11984 3 5.27976 3 4.63803 3.32698C4.07354 3.6146 3.6146 4.07354 3.32698 4.63803C3 5.27976 3 6.11984 3 7.8V16.2C3 17.8802 3 18.7202 3.32698 19.362C3.6146 19.9265 4.07354 20.3854 4.63803 20.673C5.27976 21 6.11984 21 7.8 21Z'
 							stroke='#fff'
-							stroke-width='2'
-							stroke-linecap='round'
-							stroke-linejoin='round'
+							strokeWidth='2'
+							strokeLinecap='round'
+							strokeLinejoin='round'
 						/>
 					</svg>
 				</div>
@@ -97,9 +100,9 @@ const Code = () => {
 						<path
 							d='M12 8V16M8 12H16M7.8 21H16.2C17.8802 21 18.7202 21 19.362 20.673C19.9265 20.3854 20.3854 19.9265 20.673 19.362C21 18.7202 21 17.8802 21 16.2V7.8C21 6.11984 21 5.27976 20.673 4.63803C20.3854 4.07354 19.9265 3.6146 19.362 3.32698C18.7202 3 17.8802 3 16.2 3H7.8C6.11984 3 5.27976 3 4.63803 3.32698C4.07354 3.6146 3.6146 4.07354 3.32698 4.63803C3 5.27976 3 6.11984 3 7.8V16.2C3 17.8802 3 18.7202 3.32698 19.362C3.6146 19.9265 4.07354 20.3854 4.63803 20.673C5.27976 21 6.11984 21 7.8 21Z'
 							stroke='#fff'
-							stroke-width='2'
-							stroke-linecap='round'
-							stroke-linejoin='round'
+							strokeWidth='2'
+							strokeLinecap='round'
+							strokeLinejoin='round'
 						/>
 					</svg>
 				</div>
