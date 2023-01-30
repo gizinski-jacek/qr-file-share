28214b0210be5274e05260b8877325eca62fc3db
Jacek Gizinski <jacektrg+github@gmail.com>
2023-01-30 20:41:25 +0100

    Adding socket io client.
    Listening to server socket emits.
    Fixing response error message.

--- src/components/Receive.tsx
+++ src/components/Receive.tsx
@@ -3,10 +3,20 @@ import { useEffect, useState } from 'react';
 import { QRCodeSVG } from 'qrcode.react';
 import axios, { AxiosError } from 'axios';
 import { io } from 'socket.io-client';
+import { Socket } from 'socket.io-client';
+
+export type SocketType = Socket<ServerToClientEvents>;
+
+interface ServerToClientEvents {
+	oops: (error: any) => void;
+	new_file_alert: (data: {}) => void;
+}
 
 const Receive = () => {
 	const [dirId, setDirId] = useState<string | null>(null);
 	const [codeError, setCodeError] = useState<string | null>(null);
+	const [socket, setSocket] = useState<SocketType | null>(null);
+	const [fileList, setFileList] = useState<{} | null>(null);
 
 	useEffect(() => {
 		(async () => {
@@ -14,12 +24,21 @@ const Receive = () => {
 				const res = await axios.get(
 					`${process.env.REACT_APP_API_URI}/api/receive-files`
 				);
+				const newSocket = io(`${process.env.REACT_APP_API_URI}/notifications`, {
+					query: { code: res.data },
+				});
+
 				setDirId(res.data);
+				setSocket(newSocket);
+
+				return () => {
+					newSocket.disconnect();
+				};
 			} catch (error: any) {
-				console.log(error);
+				console.error(error.message);
 				setDirId(null);
 				if (error instanceof AxiosError) {
-					setCodeError(error.message);
+					setCodeError(error.response?.data || 'Unknown server error.');
 				} else {
 					setCodeError('Unknown server error.');
 				}
@@ -27,13 +46,23 @@ const Receive = () => {
 		})();
 	}, []);
 
-	// useEffect(() => {
-	//   (() => {
-	//     // Create socket connection with dirId
-	//     // to get notified of uploaded file.
-	//     const socket = io();
-	//   })();
-	// }, [dirId]);
+	useEffect(() => {
+		if (!socket) {
+			return;
+		}
+
+		socket.on('oops', (error) => {
+			console.error(error);
+		});
+
+		socket.on('new_file_alert', (data) => {
+			setFileList(data);
+		});
+
+		return () => {
+			socket.off();
+		};
+	}, [socket]);
 
 	return !codeError ? (
 		<div className='receive'>
