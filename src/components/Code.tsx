6a66693459ba4282017581a2088c88c6c13de83d
Jacek Gizinski <jacektrg+github@gmail.com>
2023-03-20 21:13:12 +0100

    Renaming server files state.
    Adding socket io.
    Updating layout.
    Rendering file type icon.
    Converting file size to more user friendly.

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -3,6 +3,10 @@ import axios, { AxiosError } from 'axios';
 import { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSingleFile } from './Overlay';
+import { io } from 'socket.io-client';
+import { RemoteFile, SocketType } from '../types';
+import { FileIcon } from 'react-file-icon';
+import prettyBytes from 'pretty-bytes';
 
 const Code = () => {
 	const { dirId } = useParams();
@@ -12,6 +16,8 @@ const Code = () => {
 	const [success, setSuccess] = useState<boolean>(false);
 	const ref = useRef<HTMLInputElement>(null);
 	const { singleFile } = useSingleFile();
+	const [socket, setSocket] = useState<SocketType | null>(null);
+	const [remoteFiles, setRemoteFiles] = useState<RemoteFile[]>([]);
 
 	useEffect(() => {
 		setFileList(null);
@@ -21,9 +27,20 @@ const Code = () => {
 	useEffect(() => {
 		(async () => {
 			try {
-				await axios.get(
-					`${process.env.REACT_APP_API_URI}/api/code-dir-check/${dirId}`
-				);
+				if (!dirId || dirId.length !== 6) {
+				} else {
+					const res = await axios.get(
+						`${process.env.REACT_APP_API_URI}/api/code-dir-check/${dirId}`
+					);
+					console.log(res.data);
+					const newSocket = io(
+						`${process.env.REACT_APP_API_URI}/notifications`,
+						{ query: { code: dirId }, transports: ['websocket'] }
+					);
+					setRemoteFiles(res.data);
+					setSocket(newSocket);
+					return () => newSocket.disconnect();
+				}
 			} catch (error: any) {
 				console.error(error.message);
 				if (error instanceof AxiosError) {
@@ -33,7 +50,25 @@ const Code = () => {
 				}
 			}
 		})();
-	});
+	}, [dirId]);
+
+	useEffect(() => {
+		if (!socket) {
+			return;
+		}
+
+		socket.on('oops', (error) => {
+			console.error(error);
+		});
+
+		socket.on('new_file_alert', (data) => {
+			setRemoteFiles(data);
+		});
+
+		return () => {
+			socket.off();
+		};
+	}, [socket]);
 
 	const clickSelectFile = (e: React.MouseEvent<HTMLDivElement>) => {
 		e.stopPropagation();
@@ -57,6 +92,7 @@ const Code = () => {
 		}
 		setFileListErrors(null);
 		setFileList(files);
+		setSuccess(false);
 	};
 
 	const handleFileUpload = async (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -85,84 +121,110 @@ const Code = () => {
 	};
 
 	return !serverError ? (
-		<div className='send'>
-			<form encType='multipart/form-data'>
-				<label htmlFor='file'>Select file</label>
-				<input
-					ref={ref}
-					id='file'
-					name='file'
-					type='file'
-					multiple={!singleFile}
-					onChange={handleFileSelect}
-				/>
-			</form>
-			{success ? (
-				<div>Files uploaded successfully.</div>
-			) : fileList ? (
-				<div
-					className='checkmark-icon'
-					onClick={clickSelectFile}
-					aria-label='file selected'
-				>
-					<svg
-						viewBox='0 0 24 24'
-						fill='none'
-						xmlns='http://www.w3.org/2000/svg'
+		<div className='code'>
+			<div className='form-container'>
+				{fileList ? (
+					<div
+						className='checkmark-icon'
+						onClick={clickSelectFile}
+						aria-label='file selected'
 					>
-						<path
-							d='M7.5 12L10.5 15L16.5 9M7.8 21H16.2C17.8802 21 18.7202 21 19.362 20.673C19.9265 20.3854 20.3854 19.9265 20.673 19.362C21 18.7202 21 17.8802 21 16.2V7.8C21 6.11984 21 5.27976 20.673 4.63803C20.3854 4.07354 19.9265 3.6146 19.362 3.32698C18.7202 3 17.8802 3 16.2 3H7.8C6.11984 3 5.27976 3 4.63803 3.32698C4.07354 3.6146 3.6146 4.07354 3.32698 4.63803C3 5.27976 3 6.11984 3 7.8V16.2C3 17.8802 3 18.7202 3.32698 19.362C3.6146 19.9265 4.07354 20.3854 4.63803 20.673C5.27976 21 6.11984 21 7.8 21Z'
-							stroke='#fff'
-							strokeWidth='2'
-							strokeLinecap='round'
-							strokeLinejoin='round'
-						/>
-					</svg>
-				</div>
-			) : (
-				<div
-					className='add-icon'
-					onClick={clickSelectFile}
-					aria-label='select file to upload'
-				>
-					<svg
-						viewBox='0 0 24 24'
-						fill='none'
-						xmlns='http://www.w3.org/2000/svg'
+						<svg
+							viewBox='0 0 24 24'
+							fill='none'
+							xmlns='http://www.w3.org/2000/svg'
+						>
+							<path
+								d='M7.5 12L10.5 15L16.5 9M7.8 21H16.2C17.8802 21 18.7202 21 19.362 20.673C19.9265 20.3854 20.3854 19.9265 20.673 19.362C21 18.7202 21 17.8802 21 16.2V7.8C21 6.11984 21 5.27976 20.673 4.63803C20.3854 4.07354 19.9265 3.6146 19.362 3.32698C18.7202 3 17.8802 3 16.2 3H7.8C6.11984 3 5.27976 3 4.63803 3.32698C4.07354 3.6146 3.6146 4.07354 3.32698 4.63803C3 5.27976 3 6.11984 3 7.8V16.2C3 17.8802 3 18.7202 3.32698 19.362C3.6146 19.9265 4.07354 20.3854 4.63803 20.673C5.27976 21 6.11984 21 7.8 21Z'
+								stroke='#fff'
+								strokeWidth='2'
+								strokeLinecap='round'
+								strokeLinejoin='round'
+							/>
+						</svg>
+					</div>
+				) : (
+					<div
+						className='add-icon'
+						onClick={clickSelectFile}
+						aria-label='select file to upload'
 					>
-						<path
-							d='M12 8V16M8 12H16M7.8 21H16.2C17.8802 21 18.7202 21 19.362 20.673C19.9265 20.3854 20.3854 19.9265 20.673 19.362C21 18.7202 21 17.8802 21 16.2V7.8C21 6.11984 21 5.27976 20.673 4.63803C20.3854 4.07354 19.9265 3.6146 19.362 3.32698C18.7202 3 17.8802 3 16.2 3H7.8C6.11984 3 5.27976 3 4.63803 3.32698C4.07354 3.6146 3.6146 4.07354 3.32698 4.63803C3 5.27976 3 6.11984 3 7.8V16.2C3 17.8802 3 18.7202 3.32698 19.362C3.6146 19.9265 4.07354 20.3854 4.63803 20.673C5.27976 21 6.11984 21 7.8 21Z'
-							stroke='#fff'
-							strokeWidth='2'
-							strokeLinecap='round'
-							strokeLinejoin='round'
-						/>
-					</svg>
-				</div>
-			)}
-			{fileListErrors && (
-				<div>
-					{fileListErrors.map((error, i) => (
-						<div key={i}>{error}</div>
+						<svg
+							viewBox='0 0 24 24'
+							fill='none'
+							xmlns='http://www.w3.org/2000/svg'
+						>
+							<path
+								d='M12 8V16M8 12H16M7.8 21H16.2C17.8802 21 18.7202 21 19.362 20.673C19.9265 20.3854 20.3854 19.9265 20.673 19.362C21 18.7202 21 17.8802 21 16.2V7.8C21 6.11984 21 5.27976 20.673 4.63803C20.3854 4.07354 19.9265 3.6146 19.362 3.32698C18.7202 3 17.8802 3 16.2 3H7.8C6.11984 3 5.27976 3 4.63803 3.32698C4.07354 3.6146 3.6146 4.07354 3.32698 4.63803C3 5.27976 3 6.11984 3 7.8V16.2C3 17.8802 3 18.7202 3.32698 19.362C3.6146 19.9265 4.07354 20.3854 4.63803 20.673C5.27976 21 6.11984 21 7.8 21Z'
+								stroke='#fff'
+								strokeWidth='2'
+								strokeLinecap='round'
+								strokeLinejoin='round'
+							/>
+						</svg>
+					</div>
+				)}
+				{success && <div>Files uploaded successfully.</div>}
+				{fileListErrors && (
+					<div className='errors'>
+						{fileListErrors.map((error, i) => (
+							<div key={i}>{error}</div>
+						))}
+					</div>
+				)}
+				{fileList && (
+					<>
+						<div>Uploading files:</div>
+						<div className='upload-list'>
+							{Array.from(fileList).map((file, i) => {
+								return (
+									<div key={i}>
+										<FileIcon extension={file.name.slice(-3)} />
+										<div>
+											<div>{file.name}</div>
+											<div>{prettyBytes(file.size)}</div>
+										</div>
+									</div>
+								);
+							})}
+						</div>
+					</>
+				)}
+				<form encType='multipart/form-data'>
+					<label htmlFor='file'>Select file</label>
+					<input
+						ref={ref}
+						id='file'
+						name='file'
+						type='file'
+						multiple={!singleFile}
+						onChange={handleFileSelect}
+					/>
+				</form>
+				<button
+					className='send-btn'
+					type='button'
+					onClick={handleFileUpload}
+					disabled={!fileList || !!fileListErrors}
+				>
+					Send
+				</button>
+			</div>
+			{remoteFiles.length > 0 && (
+				<div className='received-file-list'>
+					{remoteFiles.map((file, i) => (
+						<div key={i}>
+							<a href={file.url} target='_blank' rel='noreferrer'>
+								<FileIcon extension={file.extension} />
+								<div>
+									<div>{file.name}</div>
+									<div>{prettyBytes(file.size)}</div>
+								</div>
+							</a>
+						</div>
 					))}
 				</div>
 			)}
-			{fileList && (
-				<div>
-					Files:{' '}
-					{Array.from(fileList).map((file, i) => {
-						return <div key={i}>{file.name}</div>;
-					})}
-				</div>
-			)}
-			<button
-				className='send-btn'
-				type='button'
-				onClick={handleFileUpload}
-				disabled={!fileList || !!fileListErrors}
-			>
-				Send
-			</button>
 		</div>
 	) : (
 		<div>{serverError}</div>
