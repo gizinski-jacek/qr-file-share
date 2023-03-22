9ae87cfbdc419a676775a2aa04d7efffeb01caaa
Jacek Gizinski <jacektrg+github@gmail.com>
2023-03-22 21:29:20 +0100

    Cleaning up tags and classes

--- src/App.tsx
+++ src/App.tsx
@@ -13,28 +13,16 @@ const App = () => {
 					path=''
 					element={
 						<div className='home'>
-							<NavLink to='send' className='img-link'>
-								<div className='send-icon' aria-label='send file'>
-									<svg
-										fill='#fff'
-										viewBox='0 0 24 24'
-										xmlns='http://www.w3.org/2000/svg'
-									>
-										<path d='M23,13v8a1,1,0,0,1-1,1H2a1,1,0,0,1-1-1V13a1,1,0,0,1,2,0v7H21V13a1,1,0,0,1,2,0ZM12,18a1,1,0,0,0,1-1V5.414l2.293,2.293a1,1,0,0,0,1.414-1.414l-4-4a1,1,0,0,0-1.414,0l-4,4A1,1,0,1,0,8.707,7.707L11,5.414V17A1,1,0,0,0,12,18Z' />
-									</svg>
-								</div>
+							<NavLink to='send' aria-label='send file'>
+								<svg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+									<path d='M23,13v8a1,1,0,0,1-1,1H2a1,1,0,0,1-1-1V13a1,1,0,0,1,2,0v7H21V13a1,1,0,0,1,2,0ZM12,18a1,1,0,0,0,1-1V5.414l2.293,2.293a1,1,0,0,0,1.414-1.414l-4-4a1,1,0,0,0-1.414,0l-4,4A1,1,0,1,0,8.707,7.707L11,5.414V17A1,1,0,0,0,12,18Z' />
+								</svg>
 								<div>Send</div>
 							</NavLink>
-							<NavLink to='receive' className='img-link'>
-								<div className='receive-icon' aria-label='receive file'>
-									<svg
-										fill='#fff'
-										viewBox='0 0 24 24'
-										xmlns='http://www.w3.org/2000/svg'
-									>
-										<path d='M7.293,13.707a1,1,0,1,1,1.414-1.414L11,14.586V3a1,1,0,0,1,2,0V14.586l2.293-2.293a1,1,0,0,1,1.414,1.414l-4,4a1,1,0,0,1-.325.216.986.986,0,0,1-.764,0,1,1,0,0,1-.325-.216ZM22,12a1,1,0,0,0-1,1v7H3V13a1,1,0,0,0-2,0v8a1,1,0,0,0,1,1H22a1,1,0,0,0,1-1V13A1,1,0,0,0,22,12Z' />
-									</svg>
-								</div>
+							<NavLink to='receive' aria-label='send file'>
+								<svg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+									<path d='M7.293,13.707a1,1,0,1,1,1.414-1.414L11,14.586V3a1,1,0,0,1,2,0V14.586l2.293-2.293a1,1,0,0,1,1.414,1.414l-4,4a1,1,0,0,1-.325.216.986.986,0,0,1-.764,0,1,1,0,0,1-.325-.216ZM22,12a1,1,0,0,0-1,1v7H3V13a1,1,0,0,0-2,0v8a1,1,0,0,0,1,1H22a1,1,0,0,0,1-1V13A1,1,0,0,0,22,12Z' />
+								</svg>
 								<div>Receive</div>
 							</NavLink>
 						</div>
@@ -42,7 +30,6 @@ const App = () => {
 				/>
 				<Route element={<Overlay />}>
 					<Route path='send' element={<Send />} />
-					{/* Remove single/multiple buttons from receive route */}
 					<Route path='receive' element={<Receive />} />
 					<Route path='code/:dirId' element={<Code />} />
 					<Route path='code' element={<Navigate to='/' replace />}></Route>
