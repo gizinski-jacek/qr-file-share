9a5746eb941f805a5c17afec9532e52a2fc8af03
Jacek Gizinski <jacektrg+github@gmail.com>
2023-03-22 21:37:11 +0100

    Checking for /receive route with useMatch to conditionally not render single/multiple buttons on that route.

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,6 +1,6 @@
 import '../styles/Overlay.scss';
 import { useState } from 'react';
-import { NavLink, Outlet, useOutletContext } from 'react-router-dom';
+import { NavLink, Outlet, useMatch, useOutletContext } from 'react-router-dom';
 
 interface ContextType {
 	singleFile: boolean;
@@ -8,6 +8,7 @@ interface ContextType {
 
 const Overlay = () => {
 	const [singleFile, setSingleFile] = useState<boolean>(true);
+	const path = useMatch('/receive');
 
 	const setSingle = () => setSingleFile(true);
 
@@ -17,47 +18,47 @@ const Overlay = () => {
 		<>
 			<Outlet context={{ singleFile }} />
 			<div className='overlay'>
-				<NavLink className='home-link' to=''>
-					<div className='arrow-icon' aria-label='home page link'>
-						<svg
-							viewBox='0 0 24 24'
-							fill='none'
-							xmlns='http://www.w3.org/2000/svg'
-						>
-							<path
-								d='M9.57 5.93018L3.5 12.0002L9.57 18.0702'
-								stroke='#fff'
-								strokeWidth='2'
-								strokeLinecap='round'
-								strokeLinejoin='round'
-							/>
-							<path
-								d='M20.4999 12H3.66992'
-								stroke='#fff'
-								strokeWidth='2'
-								strokeLinecap='round'
-								strokeLinejoin='round'
-							/>
-						</svg>
-					</div>
+				<NavLink className='home-link' aria-label='home page link' to=''>
+					<svg
+						viewBox='0 0 24 24'
+						fill='none'
+						xmlns='http://www.w3.org/2000/svg'
+					>
+						<path
+							d='M9.57 5.93018L3.5 12.0002L9.57 18.0702'
+							stroke='#fff'
+							strokeWidth='2'
+							strokeLinecap='round'
+							strokeLinejoin='round'
+						/>
+						<path
+							d='M20.4999 12H3.66992'
+							stroke='#fff'
+							strokeWidth='2'
+							strokeLinecap='round'
+							strokeLinejoin='round'
+						/>
+					</svg>
 					Home
 				</NavLink>
-				<div className='modes'>
-					<button
-						className={singleFile ? 'active' : ''}
-						type='button'
-						onClick={setSingle}
-					>
-						Single
-					</button>
-					<button
-						className={!singleFile ? 'active' : ''}
-						type='button'
-						onClick={setMultiple}
-					>
-						Multiple
-					</button>
-				</div>
+				{!path && (
+					<div className='modes'>
+						<button
+							className={singleFile ? 'active' : ''}
+							type='button'
+							onClick={setSingle}
+						>
+							Single
+						</button>
+						<button
+							className={!singleFile ? 'active' : ''}
+							type='button'
+							onClick={setMultiple}
+						>
+							Multiple
+						</button>
+					</div>
+				)}
 			</div>
 		</>
 	);
