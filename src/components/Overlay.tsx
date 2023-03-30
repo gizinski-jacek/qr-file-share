a20205b20c7e3fcc2f8d867b49102460f9bf81c0
Jacek Gizinski <jacektrg+github@gmail.com>
2023-03-30 19:32:19 +0200

    Updatig style import

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,4 +1,4 @@
-import '../styles/Overlay.scss';
+import '../styles/Overlay.module.scss';
 import { useState } from 'react';
 import { NavLink, Outlet, useMatch, useOutletContext } from 'react-router-dom';
 
